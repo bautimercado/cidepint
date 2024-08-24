66167ffdd0f95f27c559f1c94ea7f4d368855a33
Bautista Mercado <bautista.mercado184770@alumnos.info.unlp.edu.ar>
2024-08-24 01:31:32 -0300

    Subo repo

---
+++ README.md
@@ -0,0 +1,40 @@
+# CIDEPINT - 2023
+
+- [Aplicación de administración](https://admin-grupo17.proyecto2023.linti.unlp.edu.ar/)
+- [Portal público](https://grupo17.proyecto2023.linti.unlp.edu.ar/)
+
+## Admin:
+- Python 3.8.10
+- Base de datos PostgresSQL 15
+- Flask
+- Poetry
+- jinja
+- psycopg2-binary
+- Flask-SQLAlchemy
+- flask-session
+- Flask-Mail
+- flask_cors
+- flask-jwt-extended
+- flask-oauth
+- passlib
+- flask_caching
+- marshmallow
+- flask-wtf
+- wtforms
+- pytest
+- flask-shell-ipython
+
+## Portal:
+- JavaScript
+- VueJs 3.0
+- vue-router
+- axios
+- pinia
+
+## Credenciales:
+  - Super Administrador 1:
+    - Correo/Nombre de usuario: juan@admin.com
+    - Contraseña: 1234
+  - Dueño 1:
+    - Correo/Nombre de usuario: pepe.argento@mail.com
+    - Contraseña: 1234
---
+++ admin/src/web/config.py
@@ -0,0 +1,65 @@
+import os
+from os import environ
+from flask_caching import Cache
+
+
+cache = Cache(config={"CACHE_TYPE": "simple"})
+
+
+class Config(object):
+    # Clase base
+
+    SECRET_KEY = "secret"
+    TESTING = False
+    SESSION_TYPE = "filesystem"
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = 'cidepint.proyecto@gmail.com'
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    PER_PAGE = 5
+    GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+
+
+class ProductionConfig(Config):
+
+    DB_USER = environ.get("DB_USER")
+    DB_PASS = environ.get("DB_PASS")
+    DB_HOST = environ.get("DB_HOST")
+    DB_NAME = environ.get("DB_NAME")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+    )
+    URL_REGISTRO = "https://admin-grupo17.proyecto2023.linti.unlp.edu.ar/sesion/confirmar_registro"
+
+
+class DevelopmentConfig(Config):
+    # Configuracion de desarollo
+
+    DB_USER = "postgres"
+    DB_PASS = "postgres"
+    DB_HOST = "localhost"
+    DB_NAME = "postgres"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+    )
+
+    URL_REGISTRO = "http://127.0.0.1:5000/sesion/confirmar_registro"
+    URL_MAIN = "https://admin-grupo17.proyecto2023.linti.unlp.edu.ar/sesion/confirmar_registro"
+
+
+class TestingConfig(Config):
+    # Configuracion de testeo
+
+    TESTING = True
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "test": TestingConfig,
+}
